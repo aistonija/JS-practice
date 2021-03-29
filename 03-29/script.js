bfb0fee328b9089c1c5ac65110f2182447ea7699
aistonija <aistonija16@gmail.com>
2021-03-29 14:31:55 +0300

    03-29 JS praktika - strings

---
+++ 03-29/script.js
@@ -0,0 +1,57 @@
+//////////////////// JS BASICS RECAP /////////////////////
+
+// 1. Pasirasyti funkcija, kuriai padavus:
+// amziu ir limita, bus patikrinama, ar zmogus praeina sita salyga;
+
+// jeigu salyga atitiks: log 'galite praeiti'
+// jeigu salyga neatitinka: log 'jusu neileisime'
+
+// const age = prompt("Iveskite amziu");
+
+// console.log(Number(age));
+
+// const ageVerifier = (amziausParametras, verifier) => {
+//   let phrase;
+//   if (amziausParametras > verifier) {
+//     phrase = "galite praeiti";
+//   } else {
+//     phrase = "jusu neileisime";
+//   }
+
+//   return phrase;
+// };
+
+// console.log(ageVerifier(Number(age), 15));
+
+// -------------------------------------------------------
+
+// 2.
+
+// let string = "vejas man pasake vien tavo varda";
+// let arr = [];
+
+// * - check if string has more than 10 symbols in it
+// * - if yes add first and last symbol to array
+
+// patikrinti ar stringas turi daugiau nei 10 simboliu
+// jei taip - pirma ir paskutini stringo simbolius ideti i masyva
+
+// console.log(string.length);
+
+// if (string.length > 10) {
+//   arr.push(string.charAt(0));
+//   arr.push(string.charAt(string.length - 1));
+// }
+
+// console.log(arr);
+
+// -------------------------------------------------------
+
+// * - remove last element from array, add it to result string, console log both of them. use array.pop() method
+
+// * - naudojant array.pop() metoda:
+// * - is masyvo isimti paskutini elementa.
+// * - iskonsologinti result stringa su is masyvo isimtu elementu
+
+const arr = ["zuvis", "zvejyba", "ezeras", "meduolis"];
+let result = "Mmmmm koks velniskai skanus";
