6c08b0d3d3bc7115a0aca286ccbf7b489cce56d3
aistonija <aistonija16@gmail.com>
2021-03-29 14:43:42 +0300

    string concat

--- 03-29/script.js
+++ 03-29/script.js
@@ -55,3 +55,11 @@
 
 const arr = ["zuvis", "zvejyba", "ezeras", "meduolis"];
 let result = "Mmmmm koks velniskai skanus";
+// console.log(`1 budas: ${result} ${arr.pop()}`);
+
+let lastElement = arr.pop();
+let finalResult = result + " " + lastElement;
+console.log(`2 budas: ${finalResult}`);
+
+let concat = result.concat(" ", lastElement);
+console.log(`3 budas: ${concat}`);
