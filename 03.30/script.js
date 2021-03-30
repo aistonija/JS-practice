9d75c2bb6b80e4a24c327b75053cfd237c6bbdfa
aistonija <aistonija16@gmail.com>
2021-03-30 13:04:35 +0300

    object

---
+++ 03.30/script.js
@@ -0,0 +1,6 @@
+let one = {
+  numbers: [25, 87, 684, 2874, 687, 28, 68],
+  words: ["first", "second", "third", "fourth"],
+  objects: [{ one: "EMPTY" }, { two: "replace to milk", three: "full" }],
+  arrays: [[[[[[[[84, 85, 257, 577, "word", "small", "BIG"]]]]]]]],
+};
