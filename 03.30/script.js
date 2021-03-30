7b29f2c6bb7d0ed642778a0bbd4fb6c3345c0c59
aistonija <aistonija16@gmail.com>
2021-03-30 15:38:14 +0300

    5 task solution + 6 task

--- 03.30/script.js
+++ 03.30/script.js
@@ -35,7 +35,22 @@ let wordIndex = one.words.indexOf("second");
 one.words.splice(wordIndex, 1, "secondddddd");
 
 // 5. in key 'objects', make EMPTY to be in lower case, and replace its last letter 'y' with '88'.
-// Use charAt()
+// Use charAt() & replace()
 //rezultatas: objects: [{ one: "empt88" }, { two: "replace to milk", three: "full" }],
+// let word = one.objects[0].one;
+
+// console.log(word);
+
+// let letterToReplace = word.charAt(word.length - 1);
+// let newLetter = '88'
+// word = word.replace(letterToReplace, newLetter).toLowerCase();
+
+// one.objects[0].one = word;
+
+one.objects[0].one = one.objects[0].one
+  .replace(one.objects[0].one.charAt(one.objects[0].one.length - 1), "88")
+  .toLowerCase();
 
 console.log(one);
+
+// 6. in key 'arrays' make all text in lower case to be upper case and vice versa. use foreach and if.
