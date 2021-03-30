49ac2e9cee4c321399958e08000ffcdf3f0c8cd4
aistonija <aistonija16@gmail.com>
2021-03-30 14:38:52 +0300

    3 task solution + 4 task

--- 03.30/script.js
+++ 03.30/script.js
@@ -14,13 +14,21 @@ one.firstLast = [one.numbers.shift(), one.numbers.pop()];
 // 2. make something with number 2874 in 'numbers' so it becomes string and replace 28 with $$
 
 //statinis variantas
-one.numbers[2] = one.numbers[2].toString();
-one.numbers[2] = one.numbers[2].replace("28", "$$$");
+// one.numbers[2] = one.numbers[2].toString();
+// one.numbers[2] = one.numbers[2].replace("28", "$$$");
 
 // dinamiskesnis variantas
 let digit = one.numbers.indexOf(2874);
 one.numbers[digit] = one.numbers[digit].toString().replace("28", "$$$");
 
 // 3. take out last word from 'words' and put it to beginning of the array, make it to be upperCase
+
+// hint: pop(), shift(), push(), unshift();
+// words: ["FOURTH", "first", "second", "third"]
+let newObj = one.words.pop().toUpperCase();
+one.words.unshift(newObj);
+
+// 4. replace second word in 'words' array to be 'secondddddd' and add its length to the and of word.
+// Use indexOf to find where the words second stands, and then use splice method.
 
 console.log(one);
