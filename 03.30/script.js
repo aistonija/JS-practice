88a5ee225c0a2f41a073b8d7c0046974c3ef0e36
aistonija <aistonija16@gmail.com>
2021-03-30 13:29:23 +0300

    1 task solution + 2 task

--- 03.30/script.js
+++ 03.30/script.js
@@ -5,4 +5,19 @@ let one = {
   arrays: [[[[[[[[84, 85, 257, 577, "word", "small", "BIG"]]]]]]]],
 };
 
-// 1. create new key in object make it value to be first and last numbers from 'numbers' key (remove them from array)
+// 1. create new key in object
+// make it value to be first and last numbers from 'numbers' key(remove them from array)
+// hint: use shift(), pop()
+
+one.firstLast = [one.numbers.shift(), one.numbers.pop()];
+
+// Edvar variantas:
+one.twoNumber = [];
+
+one.twoNumber.push(one.numbers.shift());
+
+one.twoNumber.push(one.numbers.pop());
+
+// 2. make something with number 2874 in 'numbers' so it becomes string, and replace 28 with $$
+
+console.log(one);
