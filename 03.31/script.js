2ffde0da23c9fabe130f70f0b62c6b38da655780
aistonija <aistonija16@gmail.com>
2021-03-31 13:11:40 +0300

    1 task - kino bilietas

---
+++ 03.31/script.js
@@ -0,0 +1,12 @@
+// 1. - Kino Bilietas;
+
+// Bilieto kaina - dinamine, parametras i funkcija;
+
+// Nuolaidos:
+// - iki 6 metu: 80%;
+// - nuo 7 iki 18: 60%;
+// - nuo 19 iki 24: 20%;
+// - nuo 25 iki 64: 0%;
+// - nuo 65: 85%;
+
+// Parašyti funkciją, kuriai padavus amžių bus grąžinama bilieto kaina.
