81411110da54da0043509f7c3ee8e78c747075fa
aistonija <aistonija16@gmail.com>
2021-03-31 13:59:34 +0300

    kino bilietas - solution

--- 03.31/script.js
+++ 03.31/script.js
@@ -10,3 +10,24 @@
 // - nuo 65: 85%;
 
 // Parašyti funkciją, kuriai padavus amžių ir bilieto kainą bus grąžinama apskaičiuota bilieto kaina.
+
+const ticketPrice = (age, standardPrice) => {
+  let price;
+
+  if (age <= 6) {
+    price = standardPrice * 0.2;
+  } else if (age > 6 && age <= 18) {
+    price = standardPrice * 0.4;
+  } else if (age > 18 && age <= 24) {
+    price = standardPrice * 0.2;
+  } else if (age > 25 && age <= 64) {
+    price = standardPrice;
+  } else if (age >= 65) {
+    price = standardPrice * 0.15;
+  }
+  return price;
+};
+
+// console.log(ticketPrice(2, 10));
+
+//
