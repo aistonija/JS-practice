0a71e5517be1f80bd1ab4b2eaadbb8f420bf84d6
aistonija <aistonija16@gmail.com>
2021-04-02 13:05:37 +0300

    04.02 init

---
+++ 04.01/script.js
@@ -0,0 +1,106 @@
+const library = [
+  {
+    author: "Bill-Junior Gates MacDonalds",
+    title: "The Road Ahead",
+    readingStatus: true,
+  },
+  {
+    author: "Steve Jobs junior",
+    title: "Walter Isaacson",
+    readingStatus: true,
+  },
+  {
+    author: "Suzanne Collins ",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false,
+  },
+];
+
+// check if all readingStatus is true
+// if yes sum up all titles and console log them
+
+// if (
+//   library[0].readingStatus &&
+//   library[1].readingStatus &&
+//   library[2].readingStatus
+// ) {
+//   console.log("jega");
+// } else {
+//   console.log("nesigavo");
+// }
+
+// susikuriu flag
+let result = true;
+
+library.forEach((item) => {
+  // tikriname ar nors vienas yra false
+  if (!item.readingStatus) {
+    // jeigu bent viena false radome - bendra rezultata pakeiciame
+    result = false;
+  }
+});
+
+// console.log(result);
+
+// check whether flag variable was true ar false
+if (result) {
+  // dynamic way
+  let titles = [];
+  library.forEach((item) => titles.push(item.title));
+  console.log(titles.join(", "));
+
+  // static way
+  console.log(`${library[0].title}, ${library[1].title}, ${library[2].title} `);
+
+  // if not all readingStatus is true (at least one false):
+} else {
+  // check each object in array readingStatus separately
+  library.forEach((item) => {
+    // for those which are positive
+    if (item.readingStatus) {
+      // create array of item author name splitted by space
+      let namesArr = item.author.split(" ");
+      console.log(namesArr);
+
+      // map through array and leave only first letter
+      let initials = namesArr.map((el) => el.charAt(0));
+      initials = initials.join(".");
+      //log their authors name and surname first letters
+      console.log(initials);
+    }
+
+    // add new key "titleLength" to all objects, which represents that object title length
+    item.titleLength = item.title.length;
+  });
+}
+
+console.log(library);
+
+// check if first object in array has longer title then last object in array
+if (library[0].titleLength < library[library.length - 1].titleLength) {
+  // if no push one more object to library array with you favorite author, title, and reading status
+  library.push({
+    author: "Aiste Z",
+    title: "Mano gyvenimas",
+    readingStatus: true,
+  });
+}
+
+console.log(library);
+
+// remove first item from library array, put it to new variable "book"
+
+let book = library.shift();
+console.log(book);
+
+// add new key to variable book "bookIsNew" set it to true
+book.bookIsNew = true;
+
+// check if book.booIsNew is true and library.length is more than 2
+// if yes add book variable value to the end of library array
+
+if (book.bookIsNew && library.length > 2) {
+  library.push(book);
+}
+
+console.log(library);
