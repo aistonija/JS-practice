eba64b55a39607a65d95a96a193ce9cec639b664
aistonija <aistonija16@gmail.com>
2021-04-02 15:20:42 +0300

    Papildomos uzduotys :)

---
+++ Papildomos/script.js
@@ -0,0 +1,37 @@
+// 1. create function and do all this in it:
+
+// with single for loop remove items from wagonsWIthCoal and append them to wagonsOfGasoline
+// after this is done
+
+// check if wagonsWithCoal has any items, if not change coalCargo boolean to opposite
+// do the same with gasolineCargo key
+
+// console log modified object
+
+//-----------------------------------------------------------------
+
+// 2. Elektros saskaita
+// Tikslas: apskaiciuoti, kokia saskaita gausite uz elektra iki iskritusios dienos
+
+// kilvatvalandes kaina = 0.07;
+// menesis gali tureti nuo 29 iki 31 dienos (sveiki skaiciai)
+// saskaita galite gauti random iskritusia diena nuo 1 iki 31 (sveiki skaiciai)
+
+// kiekviena atskira diena galite suvartoti nuo 2 iki 5 kilovatvalandziu (visi skaiciai nuo 2 iki 5 per kableli, pvz. 3.25698 ir pan.)
+
+// hint: Sukti cikla PER VISAS MENESIO DIENAS, bet CIKLA SUSTABDYTI po iskritusios dienos.
+// vadinasi sukame cikla per monthDays, bet jeigu iskrito pvz 15 diena - cikla stabdome.
+
+// atvaizduoti graziai apskaiciuota suma su dviem skaiciais po kablelio
+
+//-------------------------------------------------------------------
+
+// create a function which takes array as an argument
+// go through array using loop (whichever you prefer)
+// take first letter of each word and add to aWord key in textObject
+// console log aWord key after all is done
+
+let textObject = {
+  arrayWithWords: ["firstWord", "ipad", "snow is snowing", "hamburger"],
+  aWord: "",
+};
