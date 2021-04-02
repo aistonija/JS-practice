afbf80a46041d0c938f039bbece9bb868042ddd8
aistonija <aistonija16@gmail.com>
2021-04-02 13:09:15 +0300

    1. task - flip array of booleans

---
+++ 04.02/script.js
@@ -0,0 +1,7 @@
+// go through array with for loop and flip booleans to opposite value
+// naudodami koki nors cikla, pakeiskite reiksmes i atvirkstines.
+// 1. be funkcijos
+// 2. su funkcija, kuri i parametrus priima masyva
+// 3. isbandykite ivairius ciklus
+
+let arrayOfBooleans = [true, false, false, true, true, true, false];
